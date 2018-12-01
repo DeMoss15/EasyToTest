19fd71e345478b5a3b68789575cf7e14463edfef
Daniel <mr.mossur@mail.ru>
2018-12-01 21:22:35 +0200

    simplified code

--- app/src/main/java/com/demoss/idp/domain/usecase/SaveChangesUseCase.kt
+++ app/src/main/java/com/demoss/idp/domain/usecase/SaveChangesUseCase.kt
@@ -18,77 +18,51 @@ class SaveChangesUseCase(
 
     fun save(test: TestModel): Completable = when (test.status) {
         EntityStatus.NEW -> testRepository.createTest(test).andThen(
-            saveQuestions(test.id, test.questions, EntityStatus.NEW)
+            saveQuestions(test.id, test.questions)
         )
         EntityStatus.SAVED -> Completable.complete()
         EntityStatus.MODIFIED -> {
             testRepository.updateTest(test).apply {
-                andThen( // save new questions
-                    saveQuestions(
-                        test.id,
-                        test.questions.filter { question -> question.status == EntityStatus.NEW },
-                        EntityStatus.NEW
-                    )
-                )
-                andThen( // save modified questions
-                    saveQuestions(
-                        test.id,
-                        test.questions.filter { question -> question.status == EntityStatus.MODIFIED },
-                        EntityStatus.MODIFIED
-                    )
-                )
-                andThen( // remove dropped questions
-                    saveQuestions(
-                        test.id,
-                        test.questions.filter { question -> question.status == EntityStatus.DROPPED },
-                        EntityStatus.DROPPED
-                    )
-                )
+                fun saveWithStatus(status: EntityStatus) {
+                    andThen(saveQuestions(test.id, test.questions.filter { question ->
+                        question.status == status
+                    }))
+                }
+                saveWithStatus(EntityStatus.NEW)
+                saveWithStatus(EntityStatus.MODIFIED)
+                saveWithStatus(EntityStatus.DROPPED)
             }
         }
         EntityStatus.DROPPED -> testRepository.removeTest(test)
     }.setDefaultSchedulers()
 
-    private fun saveQuestions(testId: Int, questions: List<QuestionModel>, status: EntityStatus): Completable =
+    private fun saveQuestions(testId: Int, questions: List<QuestionModel>): Completable =
         if (questions.isEmpty()) Completable.complete() else
-            when (status) {
+            when (questions.first().status) {
                 EntityStatus.NEW -> questionRepository.createQuestion(testId, *questions.toTypedArray()).andThen {
-                    questions.forEach { question -> saveAnswers(question.id, question.answers, status) }
+                    questions.forEach { question -> saveAnswers(question.id, question.answers) }
                 }
                 EntityStatus.SAVED -> Completable.complete()
                 EntityStatus.MODIFIED -> {
                     questionRepository.updateQuestion(testId, *questions.toTypedArray()).apply {
                         questions.forEach { question ->
-                            andThen( // save new answers
-                                saveAnswers(
-                                    question.id,
-                                    question.answers.filter { answer -> answer.status == EntityStatus.NEW },
-                                    EntityStatus.NEW
-                                )
-                            )
-                            andThen( // save modified answers
-                                saveAnswers(
-                                    question.id,
-                                    question.answers.filter { answer -> answer.status == EntityStatus.MODIFIED },
-                                    EntityStatus.MODIFIED
-                                )
-                            )
-                            andThen( // remove dropped answers
-                                saveAnswers(
-                                    question.id,
-                                    question.answers.filter { answer -> answer.status == EntityStatus.DROPPED },
-                                    EntityStatus.DROPPED
-                                )
-                            )
+                            fun saveWithStatus(status: EntityStatus) {
+                                andThen(saveAnswers(question.id, question.answers.filter { answer ->
+                                    answer.status == status
+                                }))
+                            }
+                            saveWithStatus(EntityStatus.NEW)
+                            saveWithStatus(EntityStatus.MODIFIED)
+                            saveWithStatus(EntityStatus.DROPPED)
                         }
                     }
                 }
                 EntityStatus.DROPPED -> questionRepository.removeQuestion(testId, *questions.toTypedArray())
             }
 
-    private fun saveAnswers(questionId: Int, answers: List<AnswerModel>, status: EntityStatus): Completable =
+    private fun saveAnswers(questionId: Int, answers: List<AnswerModel>): Completable =
         if (answers.isEmpty()) Completable.complete() else
-            when (status) {
+            when (answers.first().status) {
                 EntityStatus.NEW -> answerRepository.createAnswer(questionId, *answers.toTypedArray())
                 EntityStatus.SAVED -> Completable.complete()
                 EntityStatus.MODIFIED -> answerRepository.updateAnswer(questionId, *answers.toTypedArray())
